abb4a7225764a5deadccde481e634b3186d98e7e
Daniel Morris <daniel@danielmorris.me>
2024-06-28 00:21:32 +0000

    Remove unneccessary comments, project complete

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -13,23 +13,15 @@ import Service.MessageService;
 import java.util.List;
 
 
-/**
- * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
- * found in readme.md as well as the test cases. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- */
 public class SocialMediaController {
     ObjectMapper mapper = new ObjectMapper();
     AccountService accService = new AccountService();
     MessageService msgService = new MessageService();
-    /**
-     * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
-     * suite must receive a Javalin object from this method.
-     * @return a Javalin app object which defines the behavior of the Javalin controller.
-     */
+
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+
+        // Endpoint handling functions
         app.post("register", (context) -> this.registerUser(context));
         app.post("login", this::loginUser);
         app.post("messages", this::postMessage);
@@ -42,15 +34,6 @@ public class SocialMediaController {
         return app;
     }
 
-    /**
-     * This is an example handler for an example endpoint.
-     * @param context The Javalin Context object manages information about both the HTTP request and response.
-     */
-    private void exampleHandler(Context context) {
-        context.json("sample text");
-    }
-
-    // I don't understand why the IDE wanted me to add this throws declaration to the function?
     private void registerUser(Context context) throws JsonMappingException, JsonProcessingException {
         Account userAccount = mapper.readValue(context.body(), Account.class);
         Account resultAcc = accService.addNewAccount(userAccount);
